a9781185905b0b6ac6c32d5ce5093cf8d71cb842
dizzy <1554054818@qq.com>
2018-05-22 11:30:52 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'//移动端300毫秒点击延迟解决方案
+import './assets/styles/reset.css'//移动端样式不统一解决方案
+import './assets/styles/border.css'//移动端一像素大小解决方案
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
